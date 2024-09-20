1affe432c1d9f232c1c20147c38c0be8fa9860e1
chaudhary64 <chiragchoudhary64@gmail.com>
2024-09-20 19:01:13 +0530

    Create Nav component, remove logo.svg, and embed SVG code directly in Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Home from "./Components/Home/Home";
+import Nav from "./Components/Nav/Nav";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+      <Home />
+    </div>
+  );
 };
 
 export default App;
