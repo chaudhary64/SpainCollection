1e6a6545e6df2d033b7fdc130d814f3b60a26876
chaudhary64 <chiragchoudhary64@gmail.com>
2024-09-30 16:23:55 +0530

    Implemented Y-axis translation animation for images on scroll interaction

--- src/Components/Home/FixedImages.jsx
+++ src/Components/Home/FixedImages.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 
 const FixedImages = ({ src, number, text }) => {
+  const imageRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: imageRef,
+    offset: ["start end", "end start"],
+  });
+  const positionY = useTransform(scrollYProgress, [0, 1], [0, -70]);
   return (
-    <div className="w-72 text-lg">
-      <div
-        className="h-[400px] w-72"
-        style={{
-          backgroundImage: `url(${src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
+    <div ref={imageRef} className="w-[310px] text-lg">
+      <div className="h-[420px] w-full overflow-clip">
+        <motion.img style={{ y: positionY }} src={src} alt="" />
+      </div>
       <div className="mt-5">
         <p className="text-lg">{number}</p>
         <p className="text-base">{text}</p>
