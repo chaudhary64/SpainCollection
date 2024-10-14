0ce237d50a0b47f93756c568df5b2ddcad232755
chaudhary64 <chiragchoudhary64@gmail.com>
2024-10-14 18:09:06 +0530

    Refactor FixedImages component: Update image position, height, and text styling

--- src/Components/Home/FixedImages.jsx
+++ src/Components/Home/FixedImages.jsx
@@ -1,9 +1,5 @@
 import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useTransform,
-} from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const FixedImages = ({ src, number, text }) => {
   const imageRef = useRef(null);
@@ -11,15 +7,17 @@ const FixedImages = ({ src, number, text }) => {
     target: imageRef,
     offset: ["start end", "end start"],
   });
-  const positionY = useTransform(scrollYProgress, [0, 1], [0, -70]);
+  const positionY = useTransform(scrollYProgress, [0, 1], [0, -80]);
   return (
     <div ref={imageRef} className="w-[310px] text-lg">
-      <div className="h-[420px] w-full overflow-clip">
+      <div className="h-[70vh] w-full overflow-clip">
         <motion.img style={{ y: positionY }} src={src} alt="" />
       </div>
       <div className="mt-5">
-        <p className="text-lg">{number}</p>
-        <p className="text-base">{text}</p>
+        <p className="text-2xl text-[#0E0E0D]/95">{number}</p>
+        <p className="mt-0.5 text-xl text-[#0E0E0D]/90 tracking-wider">
+          {text}
+        </p>
       </div>
     </div>
   );
