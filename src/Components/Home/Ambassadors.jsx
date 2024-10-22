09726b5607a81f06b19bc37ed53760f1af87753a
chaudhary64 <chiragchoudhary64@gmail.com>
2024-10-22 18:03:00 +0530

    Refactor Ambassadors component: Adjust image size and add play button overlay

--- src/Components/Home/Ambassadors.jsx
+++ src/Components/Home/Ambassadors.jsx
@@ -1,15 +1,23 @@
+import { IoPlay } from "react-icons/io5";
+
 const Ambassadors = ({ src, name, text }) => {
   return (
-    <div className="w-[22%]">
-      <img className="object-cover" src={src} alt="" />
+    <div className="w-[20%]">
+      <div className="relative">
+        <img className="object-cover" src={src} alt="" />
+        <div className="absolute inset-0 bg-black/55"></div>
+        <div className="h-[72px] w-[72px] border-2 border-[#D6D6D6] rounded-full absolute top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+          <IoPlay className="text-white h-5 " />
+        </div>
+      </div>
       <div
         style={{
           mixBlendMode: "difference",
         }}
         className="mt-3 text-white"
       >
-        <p className="text-center text-xl font-[360]">{name}</p>
-        <p className="text-center text-sm font-extralight">{text}</p>
+        <p className="text-center text-2xl font-[360]">{name}</p>
+        <p className="text-center text-base font-extralight">{text}</p>
       </div>
     </div>
   );
