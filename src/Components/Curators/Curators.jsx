490eda911cc07aad5b207f8e6bc64e296a0e061a
chaudhary64 <chiragchoudhary64@gmail.com>
2024-10-29 19:57:09 +0530

    Refactor Curators component: Update styling and content

--- src/Components/Curators/Curators.jsx
+++ src/Components/Curators/Curators.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Curators = () => {
   return (
-    <div>
-      curators
+    <div
+      style={{
+        lineHeight: "0.95",
+      }}
+      className='mt-14 text-center uppercase text-white text-[107px] font-["SaolDisplay-Regular"] font-[300] bg-black'
+    >
+      <p>We work</p>
+      <p>tirelessly to</p>
+      <p>make your trip an</p>
+      <p className='text-[#BF1826] font-["SaolDisplay-Italic"]'>
+        unforgettable
+      </p>
+      <p>experience</p>
     </div>
-  )
-}
+  );
+};
 
-export default Curators
+export default Curators;
