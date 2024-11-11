519e1cf3463ba640f3a15cbcdaec323f40d98870
chaudhary64 <chiragchoudhary64@gmail.com>
2024-11-12 00:20:44 +0530

    Add play icon overlay to InfoCards component for enhanced interactivity

--- src/Components/Curators/InfoCards.jsx
+++ src/Components/Curators/InfoCards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoPlay } from "react-icons/io5";
 
 const InfoCards = ({ name, text, src, isDragging }) => {
   return (
@@ -15,7 +16,11 @@ const InfoCards = ({ name, text, src, isDragging }) => {
           className="h-[460px] w-full object-cover object-center"
           alt=""
         />
-        <div className="absolute inset-0 bg-black/60"></div>
+        <div className="absolute inset-0 flex justify-center items-center bg-black/60">
+          <div className="h-[72px] w-[72px] border-2 border-[#D6D6D6] rounded-full flex justify-center items-center">
+            <IoPlay className="text-white h-5 " />
+          </div>
+        </div>
       </div>
       <div className="mt-4 font-[Inter] tracking-wide">
         <p className="text-xl">{name}</p>
