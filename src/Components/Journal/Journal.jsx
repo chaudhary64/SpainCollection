4e9764bbfea137ce3784d17b525ff37584bf6111
chaudhary64 <chiragchoudhary64@gmail.com>
2024-11-14 14:48:09 +0530

    Improve Journal component: add descriptive text and implement initial grid layout

--- src/Components/Journal/Journal.jsx
+++ src/Components/Journal/Journal.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Journal = () => {
   return (
     <div>
-      Journal
+      {/* Text */}
+      <div className="mt-36 text-center text-[40px] font-[SaolDisplay-Light] leading-none">
+        <p>Spain Collection has selected the best places and traditions</p>
+        <p>to turn your luxury trip into an unforgettable memory</p>
+      </div>
+      {/* Grid Layout */}
+      <div className="h-20 w-full mt-24 grid grid-cols-3">
+        <div className="h-full w-full bg-red-400"></div>
+        <div className="h-full w-full bg-red-500"></div>
+        <div className="h-full w-full bg-red-700"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Journal
+export default Journal;
