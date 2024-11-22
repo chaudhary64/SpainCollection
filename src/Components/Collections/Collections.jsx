7184132922a98e3b379928535236c5a8e42a167f
chaudhary64 <chiragchoudhary64@gmail.com>
2024-11-22 17:47:45 +0530

    Add background image and text description to Collections component

--- src/Components/Collections/Collections.jsx
+++ src/Components/Collections/Collections.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Collections = () => {
   return (
-    <div>
-      collections
+    <div className="inset-0 fixed -z-10">
+      {/* Default Background Image */}
+      <div className="inset-0 absolute -z-50">
+        <img
+          src="/images/Collections/home.webp"
+          className="h-full w-full object-cover"
+          alt=""
+        />
+        <div className="inset-0 bg-black/60 absolute z-10"></div>
+      </div>
+      {/* Some Text */}
+      <div
+        style={{
+          lineHeight: "1.15",
+        }}
+        className="w-fit mx-auto mt-[16.5%] text-[40px] text-center font-[SaolDisplay-Light] text-white tracking-wide"
+      >
+        <p>Spain Collection has selected outstanding destinations, and</p>
+        <p>hand-picked experiences to make your trip unforgettable.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
