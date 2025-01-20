ef5ed7ad31306cb24f91b7330ba12e13f98b1467
chaudhary64 <chiragchoudhary64@gmail.com>
2025-01-20 12:29:41 +0530

    refactor: enhance cursor and magnet components with improved state management and animations

--- src/Components/Cursor/Cursor.jsx
+++ src/Components/Cursor/Cursor.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const Cursor = () => {
-  const [properties, setProperties] = useState({ x: 0, y: 0, scale: 0 });
+  const [properties, setProperties] = useState({
+    x: 0,
+    y: 0,
+    scale: 0,
+  });
 
   useEffect(() => {
     const handleMouseMove = (e) => {
@@ -17,32 +21,54 @@ const Cursor = () => {
       setProperties((prev) => ({ ...prev, scale: 0 }));
     };
 
+    const magnetMouseEnterHandler = () => {
+      setProperties((prev) => ({ ...prev, scale: 0 }));
+    };
+
+    const magnetMouseLeaveHandler = () => {
+      setProperties((prev) => ({ ...prev, scale: 1 }));
+    };
+
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseenter", windowMouseEnterHandler);
     document.addEventListener("mouseleave", windowMouseLeaveHandler);
+    document.querySelectorAll(`[data_target="magnet"]`).forEach((el) => {
+      el.addEventListener("mouseenter", magnetMouseEnterHandler);
+      el.addEventListener("mouseleave", magnetMouseLeaveHandler);
+    });
 
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseenter", windowMouseEnterHandler);
       document.removeEventListener("mouseleave", windowMouseLeaveHandler);
+      document.querySelectorAll(`[data_target="magnet"]`).forEach((el) => {
+        el.removeEventListener("mouseenter", magnetMouseEnterHandler);
+        el.removeEventListener("mouseleave", magnetMouseLeaveHandler);
+      });
     };
   }, []);
 
   return (
     <motion.div
       id="cursor"
+      initial={{ scale: 0, x: "-50%", y: "-50%" }}
       animate={{
-        x: properties.x - 8, // Adjust for centering (half of cursor size)
-        y: properties.y - 8,
+        x: "-50%",
+        y: "-50%",
+        left: properties.x,
+        top: properties.y,
         scale: properties.scale,
+        transition: {
+          scale: { duration: 0.3 },
+        },
       }}
       transition={{
         type: "spring", // Smooth spring animation
         stiffness: 500, // Controls speed
         damping: 30, // Controls bounce effect
       }}
-      className="h-3.5 w-3.5 fixed z-[999] rounded-full bg-[#BF1826] pointer-events-none"
-    />
+      className="h-3.5 w-3.5 flex justify-center items-center fixed z-[999] rounded-full bg-[#BF1826] pointer-events-none"
+    ></motion.div>
   );
 };
 
--- src/Components/Magnetic/Magnet.jsx
+++ src/Components/Magnetic/Magnet.jsx
@@ -1,11 +1,16 @@
 import { useContext, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { IoPlay } from "react-icons/io5";
 import { Info } from "../Context/Context";
 
 export default function Magnet() {
   const ref = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [properties, setProperties] = useState({
+    x: 0,
+    y: 0,
+    visibile: false,
+    svgOpacity: 1,
+  });
   const { screenWidth } = useContext(Info);
 
   const handleMouse = (e) => {
@@ -13,17 +18,23 @@ export default function Magnet() {
     const { height, width, left, top } = ref.current.getBoundingClientRect();
     const middleX = clientX - (left + width / 2);
     const middleY = clientY - (top + height / 2);
-    setPosition({ x: middleX, y: middleY });
+    setProperties((prev) => ({
+      x: middleX,
+      y: middleY,
+      visibile: true,
+      svgOpacity: 0,
+    }));
   };
 
   const reset = () => {
-    setPosition({ x: 0, y: 0 });
+    setProperties((prev) => ({ x: 0, y: 0, visibile: false, svgOpacity: 1 }));
   };
 
-  const { x, y } = position;
+  const { x, y, visibile, svgOpacity } = properties;
 
   return (
     <motion.div
+      data_target="magnet"
       style={{ position: "relative" }}
       ref={ref}
       onMouseMove={screenWidth > 1024 ? handleMouse : null}
@@ -31,8 +42,32 @@ export default function Magnet() {
       animate={{ x, y }}
       transition={{ type: "spring", stiffness: 150, damping: 10, mass: 0.1 }}
     >
-      <div className="h-12 xxs:h-14 lg:h-16 xl:h-20 3xl:h-28 w-12 xxs:w-14 lg:w-16 xl:w-20 3xl:w-28 border lg:border-2 border-white rounded-full flex justify-center items-center cursor-pointer">
-        <IoPlay className="h-4 lg:scale-105 xl:scale-125 3xl:scale-150 text-white" />
+      <div className="h-12 xxs:h-14 lg:h-16 xl:h-20 3xl:h-28 w-12 xxs:w-14 lg:w-16 xl:w-20 3xl:w-28 border lg:border-2 border-white rounded-full flex justify-center items-center cursor-pointer pointer-events-none relative">
+        <IoPlay
+          style={{
+            opacity: svgOpacity,
+            transition: "opacity 0.2s",
+          }}
+          className="h-4 lg:scale-105 xl:scale-125 3xl:scale-150 text-white"
+        />
+        <AnimatePresence>
+          {visibile && (
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ left: x, top: y, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 150,
+                damping: 10,
+                mass: 0.1,
+              }}
+              exit={{ scale: 0, transition: { duration: 0.3 } }}
+              className="h-[95%] w-[95%] bg-white rounded-full text-base text-black grid place-items-center absolute"
+            >
+              Play
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </motion.div>
   );
